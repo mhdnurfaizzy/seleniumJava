ae5cb2b550fc4e489ab2b06d2ccca4c7b775fa4d
mhdnurfaizzy <mhdnurfaizzy>
2024-01-14 20:25:07 +0700

    webdriver micEdge config

--- SeleniumJava/src/selenium.java
+++ SeleniumJava/src/selenium.java
@@ -1,10 +1,38 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class selenium {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");	
+
+		//Invoking Browser
+		//Chrome - ChromeDriver exten->Methods close get
+		//Firefox- FirefoxDriver ->methods close get
+		// WebDriver  close  get
+		//WebDriver methods + class methods
+		// Chrome
+
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--remote-allow-origins=*");
+//		System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
+//		WebDriver driver = new ChromeDriver(options);
+
+
+
+		//Firefox
+//		System.setProperty("webdriver.gecko.driver", "/Users/rahulshetty/Documents/geckodriver");
+//		WebDriver driver1 = new FirefoxDriver();
 		
+		//Microsoft Edge
+//		System.setProperty("webdriver.edge.driver", "/Users/rahulshetty/Documents/msedgedriver");
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+//		driver.close();
+
+		driver.quit();
 	
 	
 	}
