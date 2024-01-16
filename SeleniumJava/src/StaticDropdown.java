706bb7e6783ba611774121cfd7111df17847dfdb
mhdnurfaizzy <mhdnurfaizzy@gmial.com>
2024-01-16 16:57:31 +0700

    static dropdown and update dropdown

---
+++ SeleniumJava/src/StaticDropdown.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		//dropdown with select teg - static
+		
+		WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		
+		Select dropdown = new Select(staticDropdown);
+		dropdown.selectByIndex(3);
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		dropdown.selectByVisibleText("AED");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		dropdown.selectByValue("INR");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+
+
+		
+		
+		
+		
+		
+		
+	}
+}
