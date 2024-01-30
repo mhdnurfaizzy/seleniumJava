fc552c1687b4256054b6ae99bbf4a19d8490a7df
mhdnurfaizzy <mhdnurfaizzy@gmial.com>
2024-01-30 15:12:44 +0700

    mouseInteraction,windowsTabHandles

---
+++ SeleniumJava/ajaxCall-childWindow-iFrame/actionDemo.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class actionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.mi.co.id/id/");
+		Actions a= new Actions(driver);
+		WebElement move=driver.findElement(By.xpath("//i[@class='micon micon-account shortcut__icon']"));
+		
+		a.moveToElement(move).contextClick().build().perform();
+		a.moveToElement(driver.findElement(By.xpath("//button[@class='mi-btn mi-btn--icon mi-btn--normal mi-btn--light navigation__link']"))).click().keyDown(Keys.SHIFT).sendKeys("Redmi").doubleClick().build().perform();
+
+	}
+
+}
---
+++ SeleniumJava/ajaxCall-childWindow-iFrame/windowsHandles.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class windowsHandles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		driver.findElement(By.cssSelector(".blinkingText")).click();
+		Set<String> windows= driver.getWindowHandles(); // [parentID. chilID]
+		Iterator<String>it =windows.iterator();
+		String parentID = it.next();
+		String childID = it.next();
+		
+		driver.switchTo().window(childID);
+		System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+		driver.findElement(By.cssSelector(".im-para.red")).getText();
+		String emailID = driver.findElement(By.cssSelector(".im-para.red")).getText().split("at")[1].trim().split(" ")[0];
+		driver.switchTo().window(parentID);
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(emailID);
+//		driver.findElement(By.xpath("//input[@id='password']")).click();
+
+
+	}
+
+}
